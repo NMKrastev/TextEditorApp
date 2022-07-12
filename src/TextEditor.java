b99bfe31a1631b9044023f6517c8a7ed7c7373e4
Nikola <nikola.malinov.krastev@gmail.com>
2022-07-12 08:41:27 +0300

    Created the classes with the imports needed

---
+++ src/TextEditor.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TextEditor extends JFrame implements ActionListener {
+
+    TextEditor() {
+
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
